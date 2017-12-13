7c3691c0f7214f8d5ad1c729e22d4266c5af8007
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-12-13 18:14:37 +0100

    Implementada ruta en la que nos devolverá información detallada del evento(juerga) solicitado En este caso listará los usuarios de dicho evento

--- src/AppBundle/Controller/JuergasController.php
+++ src/AppBundle/Controller/JuergasController.php
@@ -13,11 +13,31 @@ class JuergasController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $juergas[0] = ['evento' => 'Fiesta aprobados HLC', 'asistentes'=> 17, 'fechaInicio' => '17/07/2017', 'fechaFin' => '17/07/2017', 'precioPersona' => 17.50 ];
+        $juergas[0] = ['evento' => 'Fiesta aprobados HLC', 'asistentes'=> 3, 'fechaInicio' => '17/07/2017', 'fechaFin' => '17/07/2017', 'precioPersona' => 17.50 ];
         $juergas[1] = ['evento' => 'Fiesta estudiosos', 'asistentes'=> 3, 'fechaInicio' => '19/07/2017', 'fechaFin' => '19/07/2017', 'precioPersona' => 16.00 ];
         $juergas[2] = ['evento' => 'Viaje a Despeñaperros', 'asistentes'=> 0, 'fechaInicio' => '1/08/2017', 'fechaFin' => '2/07/2017', 'precioPersona' => 50.00 ];
         return $this->render('juergas/index.html.twig', [
             'juergas' => $juergas
+        ]);
+    }
+
+
+    /**
+     * @Route("/evento/{evento}", name="eventdetail")
+     */
+    public function JuergaAction(Request $request, $evento)
+    {
+        $eventosAsistente[0] = ['evento' => 'Fiesta aprobados HLC', 'asistente'=> ['Juan','Maria', 'Diego'] ];
+        $eventosAsistente[1] = ['evento' => 'Fiesta estudiosos', 'asistente'=> ['Paco','Maria', 'Diego'] ];
+        $eventoAsistente = null;
+        foreach ($eventosAsistente as $item) {
+            if($item['evento'] == $evento) {
+                $eventoAsistente = $item;
+            }
+        }
+
+        return $this->render('juergas/eventdetail.html.twig', [
+            'juerga' => $eventoAsistente
         ]);
     }
 }
