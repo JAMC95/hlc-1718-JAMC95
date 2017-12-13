01b0a7e03f25cfdc90ec16bb9c2b5d707e4c9652
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-12-13 19:12:05 +0100

    Implementada entidad Evento con sus propiedas, getter y setter

---
+++ src/AppBundle/Entity/Evento.php
@@ -0,0 +1,130 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="evento")
+ */
+class Evento
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="date")
+     * @var \DateTime
+     */
+    private $fechaInicio;
+
+    /**
+     * @ORM\Column(type="date")
+     * @var \DateTime
+     */
+    private $fechaFin;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $precioPersona;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Evento
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return Evento
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getFechaInicio()
+    {
+        return $this->fechaInicio;
+    }
+
+    /**
+     * @param \DateTime $fechaInicio
+     * @return Evento
+     */
+    public function setFechaInicio($fechaInicio)
+    {
+        $this->fechaInicio = $fechaInicio;
+        return $this;
+    }
+
+    /**
+     * @return \Datetime
+     */
+    public function getFechaFin()
+    {
+        return $this->fechaFin;
+    }
+
+    /**
+     * @param \DateTime $fechaFin
+     * @return Evento
+     */
+    public function setFechaFin($fechaFin)
+    {
+        $this->fechaFin = $fechaFin;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrecioPersona()
+    {
+        return $this->precioPersona;
+    }
+
+    /**
+     * @param float $precioPersona
+     * @return Evento
+     */
+    public function setPrecioPersona($precioPersona)
+    {
+        $this->precioPersona = $precioPersona;
+        return $this;
+    }
+
+
+}
