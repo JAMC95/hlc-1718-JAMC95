1af6f362af7c3ca07cc544225c394002a2a9bb06
Jose Antonio Moral <joxe.bailen@gmail.com>
2018-03-10 09:54:58 +0100

    Encriptación de contraseñas

--- src/AppBundle/DataFixtures/ORM/LoadJuergasData.php
+++ src/AppBundle/DataFixtures/ORM/LoadJuergasData.php
@@ -4,14 +4,29 @@
 namespace AppBundle\DataFixtures\ORM;
 
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
+
+use AppBundle\Entity\Usuario;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Nelmio\Alice\Fixtures;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class LoadJuergasData implements FixtureInterface
+class LoadJuergasData extends Fixture
 {
+    private $userPasswordEncoder;
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
     public function load(ObjectManager $manager)
     {
-        Fixtures::load(__DIR__.'/fixtures.yml', $manager);
+        Fixtures::load(__DIR__.'/fixtures.yml', $manager,
+            [
+                'providers' => [$this]
+            ]);
+    }
+    public function codificaPassword($textoPlano)
+    {
+        return $this->userPasswordEncoder->encodePassword(new Usuario(), $textoPlano);
     }
 }
